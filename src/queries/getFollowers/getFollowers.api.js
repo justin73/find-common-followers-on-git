8f9d3cb47d3b72a3f2fb1b34d0e822a1af2d9d67
meng.jia3 <meng.jia3@ubisoft.com>
2023-01-30 22:35:06 -0500

    chore: cleanup

--- src/queries/getFollowers/getFollowers.api.js
+++ src/queries/getFollowers/getFollowers.api.js
@@ -5,13 +5,7 @@ const GET_USER_INFO_URL = `${BASE_URL}/users`;
 
 const getFollowers = async ({ username, page, pageSize }) => {
   const { data } = await axios.get(
-    `${GET_USER_INFO_URL}/${username}/followers?per_page=${pageSize}&page=${page}`,
-    {
-      headers: {
-        // here to paste your own github token
-        Authorization: `token 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`
-      }
-    }
+    `${GET_USER_INFO_URL}/${username}/followers?per_page=${pageSize}&page=${page}`
   );
 
   return data || [];
